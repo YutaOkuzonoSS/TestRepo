ec35772eadaa957661740036c9ba97cb3fbfa62b
YutaOkuzonoSS <okuzono-yuta@systemsoft.co.jp>
2019-06-10 11:27:40 +0900

    Create add.php

---
+++ add.php
@@ -0,0 +1,15 @@
+<?php
+
+class ClassName UtilCalculate 
+{
+	/**
+   * 足し算を行う
+   * @param int a 足される数
+   * @param int b 足す数
+   */
+	function add(a,b)
+	{
+		var result = a + b;
+		return result;
+	}
+}
